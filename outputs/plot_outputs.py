90533c08ecbea9269516443ec3e19ccb4410c10a
Michael Lee <michaeldlee23@gmail.com>
2020-11-02 20:16:18 -0500

    Cherrypicked horse images

--- outputs/plot_outputs.py
+++ outputs/plot_outputs.py
@@ -4,10 +4,16 @@ from PIL import Image
 
 IMAGE_DIR = sys.argv[1]
 
+# Horse cherrypicks
+# cherrypicked = ['test_1', 'test_8', 'test_12', 'test_13', 'test_16',
+#                'final_3', 'final_8', 'final_19']
+# cherrypicked = ['cifar_10_horse_' + name + '.png' for name in cherrypicked]
+
 imageList = dict()
 for (dirpath, dirnames, filenames) in os.walk(IMAGE_DIR):
   for filename in filenames:
     if 'final' in filename:
+    #if filename in cherrypicked:
       imageList[filename] = Image.open(str(os.sep.join([dirpath, filename])))
 
 r, c = 2, 5
@@ -18,9 +24,12 @@ for i in range(r):
   for j in range(c):
     axarr[i, j].axes.xaxis.set_visible(False)
     axarr[i, j].axes.yaxis.set_visible(False)
-    axarr[i, j].imshow(imageList[sortedImages[imageNum]], cmap='gray', vmin=0, vmax=255)
+    if imageNum < len(sortedImages):
+      axarr[i, j].imshow(imageList[sortedImages[imageNum]], cmap='gray', vmin=0, vmax=255)
+    else:
+      axarr[i, j].axis('off')
     imageNum += 1
 
 
-plt.savefig(IMAGE_DIR + '_outputs.png')
+plt.savefig(IMAGE_DIR + '_outputs2.png')
 plt.show()
